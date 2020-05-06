a230662ddc232ca2d18e508836d37dd93b949d38
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-05-06 16:06:38 +0530

    After declaring variables...

--- src/Main/main.java
+++ src/Main/main.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-
+        MyQueue MQ = new MyQueue();
+        Scanner sc = new Scanner(System.in);
     }
 }
